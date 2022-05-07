2faf880fce2b2b91e095abb14abcce142d05b350
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-05-07 19:26:05 -0300

    finished FeedGet mobile project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+
+import { theme } from './src/theme';
+import Widget from './src/components/Widget';
+
+interface Props {
+  children: JSX.Element;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const DissmissKeyBoard = ({ children }: Props) => {
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'height' : 'padding'}
+        style={{ flex: 1 }}
+      >
+        {children}
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <DissmissKeyBoard>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: theme.colors.background,
+        }}
+      >
+        <StatusBar style="light" backgroundColor="transparent" translucent />
+
+        <Widget />
+      </View>
+    </DissmissKeyBoard>
+  );
+}
---
+++ src/components/Widget/styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { theme } from '../../theme';
+
+export const styles = StyleSheet.create({
+  button: {
+    width: 48,
+    height: 48,
+
+    borderRadius: 24,
+    backgroundColor: theme.colors.brand,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    position: 'absolute',
+    bottom: getBottomSpace() + 16,
+    right: 16,
+  },
+
+  modal: {
+    backgroundColor: theme.colors.surface_primary,
+    paddingBottom: getBottomSpace() + 16,
+  },
+
+  indicator: {
+    backgroundColor: theme.colors.text_primary,
+    width: 56,
+    padding: 0,
+  },
+});
